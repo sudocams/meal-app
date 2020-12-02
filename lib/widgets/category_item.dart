89bfd22f11cef643faaf588f39bbd34c60669add
sudocams <yogocamlus@gmail.com>
2020-12-02 19:48:38 +0000

    fix: category error

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -6,34 +6,36 @@ class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
-  CategoryItem(this.id,this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
 
-  void selectCategory(BuildContext ctx){
+  void selectCategory(BuildContext ctx) {
     Navigator.of(ctx).pushNamed(
-      CategoryMeals.routeName, arguments: {
-        'id': id, 'title':title, 'color':color,
+      CategoryMeals.routeName,
+      arguments: {
+        'id': id,
+        'title': title,
+        'color': color,
       },
     );
-     
   }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () => selectCategory(context ) ,
+      onTap: () => selectCategory(context),
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(15),
-          child: Container(
+      child: Container(
         padding: const EdgeInsets.all(15),
-        child: Text(title, style: Theme.of(context).textTheme.headline6,),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [color.withOpacity(0.7),
-            color, 
-            ],
-            begin: Alignment.topLeft, 
-            end: Alignment.bottomRight
-          ),
+          gradient: LinearGradient(List: [
+            color.withOpacity(0.7),
+            color,
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
           borderRadius: BorderRadius.circular(15),
         ),
       ),
